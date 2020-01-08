cd4b391c5e80286b80d7650392548a88c1e5d62e
Christian Perez <perezchristian1012@gmail.com>
2020-01-08 18:54:11 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,19 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  def categories_attributes=(category_attributes)
-    category_attributes.values.each do |category_attribute|
-    category = Category.find_or_create_by(category_attribute)
+  def category_ids=(category_id_array)
+    category_id_array.reject(&:empty?).each do |id|
+      category = Category.find_by_id(id)
       self.categories << category
+    end
+  end
+
+  def categories_attributes=(category_attributes_hash)
+    category_attributes_hash.values.each do |category_attribute|
+      if key_value_pair["name"] != ""
+        category = Category.find_or_create_by(key_value_pair)
+        self.categories << category
+      end
     end
   end
 end
